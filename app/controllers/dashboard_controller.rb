530bbc86bba41b58d0bfe783683e6cd7fd7055bd
alon zuman <alonzuman7@gmail.com>
2020-04-17 13:49:50 +0300

    minior fixes

---
+++ app/controllers/dashboard_controller.rb
@@ -0,0 +1,6 @@
+class DashboardController < ApplicationController
+  def index
+    @bookings_by_me = Booking.where(user_id: current_user.id)
+    @my_cameras = Camera.where(user: current_user)
+  end
+end
