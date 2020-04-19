2ac74411cbc32c3cb351ccd05e4643d52cf9a8cc
alon zuman <alonzuman7@gmail.com>
2020-04-19 20:22:27 +0300

    styling button

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,5 +2,6 @@ class DashboardController < ApplicationController
   def index
     @bookings_by_me = Booking.where(user_id: current_user.id)
     @my_cameras = Camera.where(user: current_user)
+
   end
 end
