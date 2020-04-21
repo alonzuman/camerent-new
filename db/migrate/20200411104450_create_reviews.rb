7ddc04a204c6bc0547120645d2f809d6122ec458
Artem Blinov <temablinov@gmail.com>
2020-04-21 19:34:19 +0300

    add review view model show

--- db/migrate/20200411104450_create_reviews.rb
+++ db/migrate/20200411104450_create_reviews.rb
@@ -1,9 +1,11 @@
 class CreateReviews < ActiveRecord::Migration[5.2]
   def change
     create_table :reviews do |t|
-      t.references :user, foreign_key: true
+      # t.references :user, foreign_key: true
+      # t.references :camera, foreign_key: true
+      # t.string :content
+      t.text :content
       t.references :camera, foreign_key: true
-      t.string :content
 
       t.timestamps
     end
