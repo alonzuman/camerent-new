0a308dd677fb03d0a9dcbb89537a2f5c915a36b9
alon zuman <alonzuman7@gmail.com>
2020-04-24 12:29:06 +0300

    last fix for map

--- app/javascript/packs/toggleMapFunction.js
+++ app/javascript/packs/toggleMapFunction.js
@@ -2,20 +2,26 @@
 
 const mapToggleButton = document.querySelector('.toggle-map');
 const map = document.getElementById('map');
-const mapContainer = document.querySelector('.map-container');
+const cardsGrid = document.querySelector('.results-wrapper');
+const mapContainer = document.querySelector('#map-container');
+
+
+console.log(cardsGrid)
 
 if (window.matchMedia("(max-width: 768px)").matches) {
-  console.log('small screen');
+  // mapContainer.style.display = 'none';
+  map.style.display = 'none';
+
   mapToggleButton.addEventListener('click', () => {
-    console.log("clicked")
-    if (mapContainer.style.display == 'none') {
-      mapContainer.style.display = 'block'
-      mapToggleButton.innerText = 'Close Map'
+    if (map.style.display == 'none') {
+      cardsGrid.style.display = 'none';
+      map.style.display = 'block';
+      mapToggleButton.innerText = 'Close Map';
     } else {
-      mapContainer.style.display = 'none'
-      mapToggleButton.innerText = 'Open Map'
+      map.style.display = 'none';
+      cardsGrid.style.display = 'block';
+      mapToggleButton.innerText = 'Open Map';
     }
   })
 } else {
-  console.log('wide screen');
 }
