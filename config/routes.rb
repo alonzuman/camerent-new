8494444628402e59f669f498c8e5a191e9cb775c
Coley Schneider <coleyschneider@gmail.com>
2020-04-24 14:18:03 +0300

    viewOtherUsersProfiles

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,15 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
-  resources :users, only: [:show]
   resources :cameras do
     resources :bookings, only: [:new, :create]
     resources :reviews, only: [:new, :create, :show]
-  end  
+  end
   resources :reviews, only: [:destroy]
 
 
   get '/dashboard', to: 'dashboard#main'
+
+  get '/users/:id', to: 'profiles#show'
 
 end
